1f64e9429037a1eb7ed2d93a7e0630e2d93e4714
Jamalpur Preethika <j.preethikapreethi@gmail.com>
2025-04-30 13:35:28 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -10,4 +10,4 @@ def client():
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello World!" in response.data
+    assert b"Hello World2!" in response.data
